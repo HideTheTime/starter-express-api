79501e9e6b6c56d88fe436b7e56011948143a0f4
_____W_somnium____ <56722571+HideTheTime@users.noreply.github.com>
2022-12-21 00:27:16 +0800

    1

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const { Telegraf } = require("telegraf")
+
+const bot = new Telegraf("5660215519:AAHyiEoYZxTpM6RFe4-84FlYMV39lax3h_c")
+
+
+bot.command("oldschool", (ctx) => ctx.reply("成功了"));
+bot.command("hello", (ctx) => ctx.reply("成功了不用测试了1"));
+bot.command("test", Telegraf.reply("λ"));
+
 const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
+    bot.launch();
     res.send({hello:"hello"})
 })
 app.listen(process.env.PORT || 3000)
